7bfd984f684aed82e03bf36a7dcaaa0bd5767628
Taavi Tänavsuu <taavi.tanavsuu@lhv.ee>
2017-06-02 17:55:55 +0300

    Added starting mode in Python, and possibility to unit-test in machine with no ev3dev installation

--- folkracer.py
+++ folkracer.py
@@ -1,14 +1,26 @@
 from enum import Enum
+import ev3dev.ev3 as ev3
+from ev3dev.ev3 import Button
 
 class State(Enum):
     AWAITING_START = 0
     STARTING = 1
 
+buttons = ev3.Button()
+
 class Folkracer(object):
+
     def __init__(self,steering):
         self.steering = steering
         self.steering.initialize()
         self.state = State.AWAITING_START
+        while 1:
+            if buttons.enter:
+                self.state = State.STARTING
+                break
+            
 
     def getState(self):
         return self.state
+
+    
--- tests/folkracer_test.py
+++ tests/folkracer_test.py
@@ -1,5 +1,12 @@
 import unittest
-from unittest.mock import Mock
+from unittest.mock import Mock, MagicMock, patch
+ev3dev_mock = MagicMock()
+ev3_mock = MagicMock()
+#patch.dict("sys.modules", ev3dev.ev3=ev3Mock).start()
+import sys
+sys.modules["ev3dev"] = ev3dev_mock
+sys.modules["ev3dev.ev3"] = ev3_mock
+
 from folkracer import *
 from steering import Steering
 
@@ -25,6 +32,15 @@ class FolkracerUnitTest(unittest.TestCase):
 
         #then
         self.assertTrue(self.folkracer.getState() is State.AWAITING_START)
+
+    def test_shouldBeOnStartingModeAfterStartButtonIsPushed(self):
+        #given
+
+        #when
+        self.folkracer.buttons.on_enter('pahh')
+        
+        #then
+        self.assertTrue(self.folkracer.getState() is State.STARTING)
         
 if __name__ == '__main__':
     unittest.main()
