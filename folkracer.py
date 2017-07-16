126053fe164348347dbb4428855afdeb1874438e
k6ps <tanavst@gmail.com>
2017-07-16 22:43:10 +0300

    Added various dependencies, added start button plressing.

--- folkracer.py
+++ folkracer.py
@@ -1,26 +1,36 @@
 from enum import Enum
-import ev3dev.ev3 as ev3
-from ev3dev.ev3 import Button
+#import ev3dev.ev3 as ev3
+#from ev3dev.ev3 import Button
 
 class State(Enum):
     AWAITING_START = 0
     STARTING = 1
 
-buttons = ev3.Button()
+#buttons = ev3.Button()
 
 class Folkracer(object):
 
-    def __init__(self,steering):
+    def __init__(self, steering, engine, distances, buttons, settings, orientation, notifications, log):
+
         self.steering = steering
         self.steering.initialize()
+        self.engine = engine
+        self.distances = distances
+        self.buttons = buttons
+        self.buttons.addStartButtonListener(self)
+        self.settings = settings
+        self.orientation = orientation
+        self.notifications = notifications
+        self.log = log
         self.state = State.AWAITING_START
-        while 1:
-            if buttons.enter:
-                self.state = State.STARTING
-                break
-            
+        #while 1:
+        #   if buttons.enter:
+        #       break
 
     def getState(self):
         return self.state
+
+    def startButtonPressed(self):
+       self.state = State.STARTING
 
     
--- tests/folkracer_test.py
+++ tests/folkracer_test.py
@@ -1,11 +1,10 @@
 import unittest
 from unittest.mock import Mock, MagicMock, patch
-ev3dev_mock = MagicMock()
-ev3_mock = MagicMock()
-#patch.dict("sys.modules", ev3dev.ev3=ev3Mock).start()
-import sys
-sys.modules["ev3dev"] = ev3dev_mock
-sys.modules["ev3dev.ev3"] = ev3_mock
+#ev3dev_mock = MagicMock()
+#ev3_mock = MagicMock()
+#import sys
+#sys.modules["ev3dev"] = ev3dev_mock
+#sys.modules["ev3dev.ev3"] = ev3_mock
 
 from folkracer import *
 from steering import Steering
@@ -15,7 +14,10 @@ class FolkracerUnitTest(unittest.TestCase):
     def setUp(self):
         self.steering = Steering()
         self.steering.initialize = Mock()
-        self.folkracer = Folkracer(self.steering)
+        self.engine = MagicMock()
+        self.distances = MagicMock()
+        self.buttons = MagicMock()
+        self.folkracer = Folkracer(self.steering, self.engine, self.distances, self.buttons, MagicMock(), MagicMock(), MagicMock(), MagicMock())
 
     def test_shouldInitializeSteeringOnStartup(self):
         #given
@@ -25,6 +27,14 @@ class FolkracerUnitTest(unittest.TestCase):
         #then
         self.steering.initialize.assert_called()
 
+    def test_shouldRegisterStartButtonListenerOnStartup(self):
+        #given
+
+        #when
+
+        #then
+        self.buttons.addStartButtonListener.assert_called_with(self.folkracer)
+
     def test_shouldBeOnAwaitingStartStateOnStartup(self):
         #given
 
@@ -33,11 +43,11 @@ class FolkracerUnitTest(unittest.TestCase):
         #then
         self.assertTrue(self.folkracer.getState() is State.AWAITING_START)
 
-    def test_shouldBeOnStartingModeAfterStartButtonIsPushed(self):
+    def test_shouldBeOnStartingModeWhenStartButtonPressed(self):
         #given
 
         #when
-        self.folkracer.buttons.on_enter('pahh')
+        self.folkracer.startButtonPressed()
         
         #then
         self.assertTrue(self.folkracer.getState() is State.STARTING)
