c5b259bd6a3db89831e87dd22dfe0fb10ae7d57a
Taavi Tänavsuu <taavi.tanavsuu@lhv.ee>
2017-11-19 15:33:28 +0200

    Implemented set_steering_position

--- steering.py
+++ steering.py
@@ -18,12 +18,17 @@ class Steering:
         self.steering_motor_speed_factor = settings.getSteeringMotorSpeedFactor()
         self.steering_speed = settings.getSteeringSpeed()
         self.steering_max_range = settings.getSteeringMaxRange()
+        self.steering_motor_speed = self.steering_speed * self.steering_motor_speed_factor
 
-    def __experimentally_find_max_left_position(self, steering_speed):
+    def __experimentally_find_max_left_position(self):
         __previous_position = self.steering_motor.position
         __position_has_not_changed_counter = 0
         for i in range(0, INITIALIZE_EXPERIMENTAL_TURN_MAX_CYCLE_COUNT):
-            self.steering_motor.run_to_rel_pos(position_sp=INITIALIZE_EXPERIMENTAL_TURN_POSITION_SP, speed_sp=steering_speed, stop_action='hold')
+            self.steering_motor.run_to_rel_pos(
+                position_sp=INITIALIZE_EXPERIMENTAL_TURN_POSITION_SP,
+                speed_sp=self.steering_motor_speed,
+                stop_action='hold'
+            )
             time.sleep(INITIALIZE_EXPERIMENTAL_TURN_SLEEP_BETWEEN_CYCLES_SEC)
             if (__previous_position == self.steering_motor.position):
                 __position_has_not_changed_counter = __position_has_not_changed_counter + 1
@@ -34,13 +39,12 @@ class Steering:
                 break
 
     def initialize(self):
-        __steering_speed = self.steering_speed * self.steering_motor_speed_factor
         logging.debug('initialize: beginning steering motor position = '+ str(self.steering_motor.position))
-        self.__experimentally_find_max_left_position(__steering_speed)
+        self.__experimentally_find_max_left_position()
         logging.debug('initialize: steering motor position after experimentally turning max left = '+ str(self.steering_motor.position))
         self.steering_motor.run_to_rel_pos(
             position_sp=-(self.steering_max_range + INITIALIZE_RETURN_TO_CENTER_COMPENSATE_POSITION_SP),
-            speed_sp=__steering_speed,
+            speed_sp=self.steering_motor_speed,
             stop_action='hold'
         )
         time.sleep(1)
@@ -53,5 +57,13 @@ class Steering:
         logging.debug('get_current_steering_position: steering position = '+ str(__current_steering_position))
         return __current_steering_position
 
-    def set_steering_position(self, steering):
-        logging.debug('setSteeringPosition: ' + str(steering))
+    def set_steering_position(self, desired_steering_position):
+        logging.debug('set_steering_position: desired_steering_position = ' + str(desired_steering_position))
+        logging.debug('set_steering_position: steering motor position = ' + str(self.steering_motor.position))
+        __desired_steering = int(desired_steering_position * self.steering_max_range / 100) - self.steering_motor.position
+        logging.debug('set_steering_position: desired_steering = ' + str(__desired_steering))
+        self.steering_motor.run_to_rel_pos(
+            position_sp=__desired_steering,
+            speed_sp=self.steering_motor_speed,
+            stop_action='hold'
+        )
--- tests/steering_test.py
+++ tests/steering_test.py
@@ -161,3 +161,105 @@ class SteeringUnitTest(unittest.TestCase):
         # then
         self.steering.steering_motor.run_to_rel_pos.assert_called_with(position_sp=-(47 + 7), speed_sp=(10 * 123), stop_action='hold')
 
+    def test_should_turn_steering_motor_left_by_correct_amount_when_steering_motor_position_was_zero(self):
+        # given
+        self.settings.getSteeringMotorAddress.return_value = 'steering_motor'
+        self.settings.getSteeringMotorSpeedFactor.return_value = 123
+        self.settings.getSteeringSpeed.return_value = 10
+        self.settings.getSteeringMaxRange.return_value = 47
+        self.steering = Steering(self.settings)
+        self.steering.steering_motor = MagicMock()
+        self.steering.steering_motor.position = 0
+
+        # when
+        self.steering.set_steering_position(-50)
+
+        # then
+        __expected_steering_motor_turn = int(-50 * 47 / 100)
+        self.steering.steering_motor.run_to_rel_pos.assert_called_with(position_sp=__expected_steering_motor_turn, speed_sp=(10 * 123), stop_action='hold')
+
+    def test_should_turn_steering_motor_right_by_correct_amount_when_steering_motor_position_was_zero(self):
+        # given
+        self.settings.getSteeringMotorAddress.return_value = 'steering_motor'
+        self.settings.getSteeringMotorSpeedFactor.return_value = 123
+        self.settings.getSteeringSpeed.return_value = 10
+        self.settings.getSteeringMaxRange.return_value = 47
+        self.steering = Steering(self.settings)
+        self.steering.steering_motor = MagicMock()
+        self.steering.steering_motor.position = 0
+
+        # when
+        self.steering.set_steering_position(66)
+
+        # then
+        __expected_steering_motor_turn = int(66 * 47 / 100)
+        self.steering.steering_motor.run_to_rel_pos.assert_called_with(position_sp=__expected_steering_motor_turn, speed_sp=(10 * 123), stop_action='hold')
+
+    def test_should_turn_steering_motor_left_by_correct_amount_when_steering_motor_position_was_in_left(self):
+        # given
+        self.settings.getSteeringMotorAddress.return_value = 'steering_motor'
+        self.settings.getSteeringMotorSpeedFactor.return_value = 123
+        self.settings.getSteeringSpeed.return_value = 10
+        self.settings.getSteeringMaxRange.return_value = 47
+        self.steering = Steering(self.settings)
+        self.steering.steering_motor = MagicMock()
+        self.steering.steering_motor.position = -10
+
+        # when
+        self.steering.set_steering_position(-50)
+
+        # then
+        __expected_steering_motor_turn = int(-50 * 47 / 100) + 10
+        self.steering.steering_motor.run_to_rel_pos.assert_called_with(position_sp=__expected_steering_motor_turn, speed_sp=(10 * 123), stop_action='hold')
+
+    def test_should_turn_steering_motor_left_by_correct_amount_when_steering_motor_position_was_in_right(self):
+        # given
+        self.settings.getSteeringMotorAddress.return_value = 'steering_motor'
+        self.settings.getSteeringMotorSpeedFactor.return_value = 123
+        self.settings.getSteeringSpeed.return_value = 10
+        self.settings.getSteeringMaxRange.return_value = 47
+        self.steering = Steering(self.settings)
+        self.steering.steering_motor = MagicMock()
+        self.steering.steering_motor.position = 15
+
+        # when
+        self.steering.set_steering_position(-50)
+
+        # then
+        __expected_steering_motor_turn = int(-50 * 47 / 100) - 15
+        self.steering.steering_motor.run_to_rel_pos.assert_called_with(position_sp=__expected_steering_motor_turn, speed_sp=(10 * 123), stop_action='hold')
+
+    def test_should_turn_steering_motor_right_by_correct_amount_when_steering_motor_position_was_in_right(self):
+        # given
+        self.settings.getSteeringMotorAddress.return_value = 'steering_motor'
+        self.settings.getSteeringMotorSpeedFactor.return_value = 123
+        self.settings.getSteeringSpeed.return_value = 10
+        self.settings.getSteeringMaxRange.return_value = 47
+        self.steering = Steering(self.settings)
+        self.steering.steering_motor = MagicMock()
+        self.steering.steering_motor.position = 15
+
+        # when
+        self.steering.set_steering_position(60)
+
+        # then
+        __expected_steering_motor_turn = int(60 * 47 / 100) - 15
+        self.steering.steering_motor.run_to_rel_pos.assert_called_with(position_sp=__expected_steering_motor_turn, speed_sp=(10 * 123), stop_action='hold')
+
+    def test_should_turn_steering_motor_right_by_correct_amount_when_steering_motor_position_was_in_left(self):
+        # given
+        self.settings.getSteeringMotorAddress.return_value = 'steering_motor'
+        self.settings.getSteeringMotorSpeedFactor.return_value = 123
+        self.settings.getSteeringSpeed.return_value = 10
+        self.settings.getSteeringMaxRange.return_value = 47
+        self.steering = Steering(self.settings)
+        self.steering.steering_motor = MagicMock()
+        self.steering.steering_motor.position = -20
+
+        # when
+        self.steering.set_steering_position(60)
+
+        # then
+        __expected_steering_motor_turn = int(60 * 47 / 100) + 20
+        self.steering.steering_motor.run_to_rel_pos.assert_called_with(position_sp=__expected_steering_motor_turn, speed_sp=(10 * 123), stop_action='hold')
+
