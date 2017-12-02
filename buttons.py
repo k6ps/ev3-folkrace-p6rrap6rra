bc3001bf88411ca49489dfa13fc71f65ef270259
Taavi Tänavsuu <taavi.tanavsuu@lhv.ee>
2017-12-02 18:02:45 +0200

    Added stop button listener

--- buttons.py
+++ buttons.py
@@ -10,29 +10,48 @@ class Buttons(Thread):
         self.stop_command_received = False
         self.ev3_buttons = ev3.Button()
         self.start_button_listener = None
+        self.stop_button_listener = None
 
     def _anyArrowButtonPressed(self):
         return (self.ev3_buttons.up | self.ev3_buttons.down | self.ev3_buttons.left | self.ev3_buttons.right)
 
+    def _backButtonPressed(self):
+        return self.ev3_buttons.backspace
+
     def _hasStartButtonListener(self):
         return (self.start_button_listener != None)
         
+    def _hasStopButtonListener(self):
+        return not (self.stop_button_listener is None)
+
     def addStartButtonListener(self, start_button_listener):
-        logging.debug('Start button listener added')
+        logging.debug('Buttons: start button listener added')
         self.start_button_listener = start_button_listener
 
+    def addStopButtonListener(self, stop_button_listener):
+        logging.debug('Buttons: stop button listener added')
+        self.stop_button_listener = stop_button_listener
+
     def removeStartButtonListener(self):
-        logging.debug('Start button listener removed')
+        logging.debug('Buttons: start button listener removed')
         self.start_button_listener = None
 
+    def removeStopButtonListener(self):
+        logging.debug('Buttons: stop button listener removed')
+        self.stop_button_listener = None
+
     def run(self):
-        logging.debug('Buttons started.')
+        logging.debug('Buttons: buttons started.')
         while (self.stop_command_received == False):
-            if (self._hasStartButtonListener() & self._anyArrowButtonPressed()):
-                logging.debug('Any arrow button pressed and start button listener is present')
+            if (self._hasStartButtonListener() and self._anyArrowButtonPressed()):
+                logging.debug('Buttons: any arrow button pressed and start button listener is present')
                 self.start_button_listener.startButtonPressed()
-        logging.debug('Buttons stopped.')
+            if (self._hasStopButtonListener() and self._backButtonPressed()):
+                logging.debug('Buttons: back button pressed and stop button listener is present')
+                self.stop_button_listener.stopButtonPressed()
+        logging.debug('Buttons: buttons stopped.')
 
     def stop(self):
+        logging.debug('Buttons: stop requested.')
         self.stop_command_received = True
 
--- tests/buttons_test.py
+++ tests/buttons_test.py
@@ -11,12 +11,14 @@ class ButtonsUnitTest(unittest.TestCase):
     def setUp(self):
         self.buttons = Buttons()
         self.start_button_listener = MagicMock()
+        self.stop_button_listener = MagicMock()
         self.ev3_buttons = MagicMock()
         self.buttons.ev3_buttons = self.ev3_buttons
         self.ev3_buttons.up = False
         self.ev3_buttons.down = False
         self.ev3_buttons.left = False
         self.ev3_buttons.right = False
+        self.ev3_buttons.backspace = False
         self.buttons.start()
 
     def _emulateUpButtonClick(self):
@@ -39,6 +41,11 @@ class ButtonsUnitTest(unittest.TestCase):
         time.sleep(0.01)
         self.ev3_buttons.right = False
 
+    def _emulateBackButtonClick(self):
+        self.ev3_buttons.backspace = True
+        time.sleep(0.01)
+        self.ev3_buttons.backspace = False
+
     def tearDown(self):
         self.buttons.stop()
 
@@ -55,7 +62,7 @@ class ButtonsUnitTest(unittest.TestCase):
     def test_shouldNotFireStartButtonPressedWhenNoStartButtonListenerAddedAndEV3UpButtonPressed(self):
         #given
 
-        #when 
+        #when
         self._emulateUpButtonClick()
 
         #then
@@ -161,6 +168,95 @@ class ButtonsUnitTest(unittest.TestCase):
 
         #then
         self.start_button_listener.startButtonPressed.assert_not_called()
+
+    def test_should_not_fire_start_button_pressed_when_EV3_back_button_pressed(self):
+        # given
+        self.buttons.addStartButtonListener(self.start_button_listener)
+        self.buttons.addStopButtonListener(self.stop_button_listener)
+
+        # when
+        self._emulateBackButtonClick()
+
+        # then
+        self.start_button_listener.startButtonPressed.assert_not_called()
+
+    def test_should_not_fire_stop_button_pressed_when_no_stop_button_listener_added(self):
+        # given
+        self.buttons.addStartButtonListener(self.start_button_listener)
+
+        # when
+        self._emulateBackButtonClick()
+
+        # then
+        self.stop_button_listener.stopButtonPressed.assert_not_called()
+
+    def test_should_not_fire_stop_button_pressed_when_stop_button_listener_removed(self):
+        # given
+        self.buttons.addStartButtonListener(self.start_button_listener)
+        self.buttons.addStopButtonListener(self.stop_button_listener)
+        self.buttons.removeStopButtonListener()
+
+        # when
+        self._emulateBackButtonClick()
+
+        # then
+        self.stop_button_listener.stopButtonPressed.assert_not_called()
+
+    def test_should_not_fire_stop_button_pressed_when_EV3_up_button_pressed(self):
+        # given
+        self.buttons.addStartButtonListener(self.start_button_listener)
+        self.buttons.addStopButtonListener(self.stop_button_listener)
+
+        # when
+        self._emulateUpButtonClick()
+
+        # then
+        self.stop_button_listener.stopButtonPressed.assert_not_called()
+
+    def test_should_not_fire_stop_button_pressed_when_EV3_down_button_pressed(self):
+        # given
+        self.buttons.addStartButtonListener(self.start_button_listener)
+        self.buttons.addStopButtonListener(self.stop_button_listener)
+
+        # when
+        self._emulateDownButtonClick()
+
+        # then
+        self.stop_button_listener.stopButtonPressed.assert_not_called()
+
+    def test_should_not_fire_stop_button_pressed_when_EV3_left_button_pressed(self):
+        # given
+        self.buttons.addStartButtonListener(self.start_button_listener)
+        self.buttons.addStopButtonListener(self.stop_button_listener)
+
+        # when
+        self._emulateLeftButtonClick()
+
+        # then
+        self.stop_button_listener.stopButtonPressed.assert_not_called()
+
+    def test_should_not_fire_stop_button_pressed_when_EV3_right_button_pressed(self):
+        # given
+        self.buttons.addStartButtonListener(self.start_button_listener)
+        self.buttons.addStopButtonListener(self.stop_button_listener)
+
+        # when
+        self._emulateRightButtonClick()
+
+        # then
+        self.stop_button_listener.stopButtonPressed.assert_not_called()
+
+    def test_should_fire_stop_button_pressed_when_EV3_back_button_pressed(self):
+        # given
+        self.buttons.addStartButtonListener(self.start_button_listener)
+        self.buttons.addStopButtonListener(self.stop_button_listener)
+
+        # when
+        self._emulateBackButtonClick()
+
+        # then
+        self.stop_button_listener.stopButtonPressed.assert_called()
+
 
 if __name__ == '__main__':
     unittest.main()
